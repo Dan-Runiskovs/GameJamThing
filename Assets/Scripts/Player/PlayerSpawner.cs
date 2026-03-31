7793f4390d4f5bc93f15048f369383fc7235f1fe
Dan Runiskovs <dan.runiskovs@student.howest.be>
2026-03-31 14:13:14 +0200

    Added a player spawner
    Player spawner spawns players based upon how many gamepads are connected, and in predefined positions

---
+++ Assets/Scripts/Player/PlayerSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerPrefab;
+
+    [Header("Player Spawn positions (in order of spawning)")]
+    [SerializeField] private List<Transform> playerSpawnPoints = new List<Transform>();
+
+    public static event System.Action PlayersSpawned;
+
+    private void Start()
+    {
+        var gamepads = Gamepad.all;
+
+        for (int i = 0; i < Mathf.Min(4, gamepads.Count); i++)
+        {
+            PlayerInput player = PlayerInput.Instantiate(
+                playerPrefab.gameObject,
+                playerIndex: i,
+                pairWithDevice: gamepads[i]
+            );
+            // --- Move player to spawn point ---
+            player.GetComponent<Rigidbody>().position = playerSpawnPoints[i].position;
+            player.GetComponent<Rigidbody>().rotation = playerSpawnPoints[i].rotation;
+        }
+        PlayersSpawned?.Invoke();
+    }
+}
