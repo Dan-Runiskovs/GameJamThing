7c36aec525a6340f4b56b5161a7a0daeb635c2cd
alexconmar <alex.concellon.marimon@student.howest.be>
2026-04-01 15:43:10 +0200

    UI stuff

---
+++ Assets/Scripts/GameStates&UI/OptionsManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionsManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
